722a3f5f2d361537105c0c924a709b104fc6beaa
panyilin <406612515@qq.com>
2020-07-16 10:53:09 +0800

    2020年7月16日10:53:07

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,12 +1,62 @@
 $(function () {
+    // 登录事件
     $('#linkreg').on('click', function () {
         $('.loginBox').hide();
         $('.regBox').show();
     })
+    // 注册事件
     $('#linklogin').on('click', function () {
         $('.regBox').hide();
         $('.loginBox').show();
     })
+    // 监听注册提交事件
+    $('#regForm').on('submit', function (e) {
+        let password = $('.regBox [name="password"]').val();
+        let username = $('.regBox [name="username"]').val();
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "/api/reguser",
+            data: {
+                username,
+                password
+            },
+            success: function (res) {
+                console.log(res);
+                if (res.status != 0) return layer.msg(res.message);
+                layer.msg(res.message);
+                $('.regBox [name="password"]').val('');
+                $('.regBox [name="username"]').val('');
+                $('.regBox [name="repassword"]').val('');
+
+                $('#linklogin').click()
+            }
+        });
+        // return false;
+    })
+    // 监听登录提交事件
+    $('#loginForm').on('submit', function () {
+        // return false;
+        
+        $.ajax({
+            type: "post",
+            url: "/api/login",
+            data: $(this).serialize(),
+            success: function (res) {
+                console.log(res);
+                if (res.status != 0) return layer.msg(res.message + '账号密码错误');
+                layer.msg(res.message);
+                localStorage.setItem('token',res.token)
+                // 跳转后台主页
+                // console.log(location.href);
+                // location.href='/index.html'
+            }
+        });
+        return false;
+    })
+
+
+    // 正则判断
     layui.form.verify({
         username: function (value, item) { //value：表单的值、item：表单的DOM对象
             if (!new RegExp("^[a-zA-Z0-9_\u4e00-\u9fa5\\s·]+$").test(value)) {
@@ -25,7 +75,15 @@ $(function () {
         , pass: [
             /^[\S]{6,12}$/
             , '密码必须6到12位，且不能出现空格'
-        ]
+        ], repass: function () {
+            let val = $('.regBox [name="password"]').val();
+            let reVal = $('.regBox [name="repassword"]').val();
+            // console.log(val, reVal);
+            if (val != reVal) {
+                return '两次密码不相同'
+            }
+        }
+
     })
 
 })
