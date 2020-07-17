e9597cc7618da77a19c6944c893a791960db998c
panyilin <406612515@qq.com>
2020-07-18 00:56:50 +0800

    art_list 完全功能 2020年7月18日00:56:44

--- assets/js/article.js/atr_list.js
+++ assets/js/article.js/atr_list.js
@@ -6,7 +6,8 @@ $(function () {
         cate_id: '',//	文章分类的 Id
         state: ''//	文章的状态
     }
-    var form = layui.form
+    var form = layui.form;
+    var laypage = layui.laypage;
     getdataTable()
     // 渲染页面
     function getdataTable() {
@@ -16,10 +17,12 @@ $(function () {
             data: q,
             // dataType: "dataType",
             success: function (res) {
-                console.log(res);
+                // console.log(res);
                 if (res.status != 0) return layui.layer.msg('获取失败');
                 var newdata = template('tbd', res)
                 $('#tbody').html(newdata)
+                // 调用渲染分页的方法
+                renderPage(res.total)
             }
         });
     }
@@ -60,25 +63,67 @@ $(function () {
     getClassification();
     // 监听分类获取渲染事件
     // 可否用click事件？
-    $('.layui-form').click(function (e) {
+    $('.layui-form').submit(function (e) {
         e.preventDefault();
         q.cate_id = $('[name="cate_id"]').val();
         q.state = $('[name="state"]').val();
-        console.log(q);
+        // console.log(q);
         getdataTable()
     })
-    // 删除请求事件
-    $('tbody').on('click','.del',function(){
-        var id=$(this).prop('id');
-        $.ajax({
-            type: "method",
-            url: "url",
-            data: "data",
-            dataType: "dataType",
-            success: function (response) {
-                
+
+    // 定义渲染分页的方法
+    function renderPage(num) {
+        // console.log(num);
+        laypage.render({
+            elem: 'pagbox' //注意，这里的 test1 是 ID，不用加 # 号
+            , count: num //数据总数，从服务端得到
+            , limit: q.pagesize
+            , curr: q.pagenum
+            , limits: [1, 2, 3, 4, 5]
+            , layout: ['count', 'prev', 'page', 'next', 'limit', 'skip']
+            , jump: function (obj, first) {
+                //obj包含了当前分页的所有参数，比如：
+                // console.log(obj.curr); //得到当前页，以便向服务端请求对应页的数据。
+                // console.log(obj.limit); //得到每页显示的条数
+                q.pagenum = obj.curr;
+                q.pagesize = obj.limit;
+
+                //首次不执行
+                if (!first) {
+                    //do something
+                    getdataTable()
+                }
             }
+        })
+    }
+    // 删除请求事件
+    $('tbody').on('click', '.del', function () {
+        // 为啥这个是当前显示的页面个数不是总数呢？
+        let len = $('.del').length;
+        // console.log(len);
+        var id = $(this).attr('data-id');
+        layer.confirm('是否删除', { icon: 3, title: '提示' }, function (index) {
+            //do something
+            $.ajax({
+                type: "get",
+                url: "/my/article/delete/" + id,
+                // dataType: "dataType",
+                success: function (res) {
+                    // console.log(res);
+                    if (res.status != 0) return layui.layer.msg('删除失败')
+                    getdataTable()
+                    console.log(len);
+                    if (len == 1) {
+                        // console.log(11);
+                        q.pagenum = q.pagenum == 1 ? 1 : q.pagenum - 1;
+                        getdataTable()
+                    }
+                }
+            });
+            layer.close(index);
         });
+        // console.log(id);
+
     })
 
 })
